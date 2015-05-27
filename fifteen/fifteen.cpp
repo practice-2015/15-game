92b36843fad4c68fde616e455133cf04f138930d
semchyshyn-om <semchyshyn_om@fizmat.tnpu.edu.ua>
2015-05-27 15:18:07 +0300

    Update fifteen.cpp

--- fifteen/fifteen.cpp
+++ fifteen/fifteen.cpp
@@ -5,8 +5,41 @@
 #include "iostream"
 using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
+void PrintTable()
 {
+	int i, w = 8;
+
+
+	const unsigned char Single_Top_Left = 218;
+	const unsigned char Single_Top_Right = 191;
+	const unsigned char Single_Bottom_Left = 192;
+	const unsigned char Single_Bottom = 193;
+	const unsigned char Single_Bottom_Right = 217;
+	const unsigned char Single_Center_Left = 195;
+	const unsigned char Single_Center_Center = 197;
+	const unsigned char Single_Center_Right = 180;
+	const unsigned char Single_Horz = 196;
+	const unsigned char Single_Vert = 179;
+	const unsigned char Single_Center = 194;
+	const unsigned char Single = 0;
+	
+	cout << Single_Top_Left;  
+	for (i = 1; i <= w - 2; i++)
+		cout << Single_Horz;
+	cout << Single_Center;
+	for (i = 1; i <= w - 2; i++)
+		cout << Single_Horz;
+	cout << Single_Center;
+	for (i = 1; i <= w - 2; i++)
+		cout << Single_Horz;
+	cout << Single_Center;
+	for (i = 1; i <= w - 2; i++)
+		cout << Single_Horz;
+	cout << Single_Top_Right << endl;
+}
+int _tmain(int argc, _TCHAR* argv[])
+{ 
+	PrintTable();
 	return 0;
 
 }
