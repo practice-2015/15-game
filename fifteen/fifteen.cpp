5821bcad4c47be4ca1f09e5033adb7288d669347
semchyshyn-om <semchyshyn_om@fizmat.tnpu.edu.ua>
2015-05-28 10:41:46 +0300

    Update fifteen.cpp

--- fifteen/fifteen.cpp
+++ fifteen/fifteen.cpp
@@ -348,6 +348,33 @@ bool IsCorrect()
 	}
 	return Yes;
 }
+
+enum ConsoleColor
+{
+	Black = 0,
+	Blue = 1,
+	Green = 2,
+	Cyan = 3,
+	Red = 4,
+	Magenta = 5,
+	Brown = 6,
+	LightGray = 7,
+	YesrkGray = 8,
+	LightBlue = 9,
+	LightGreen = 10,
+	LightCyan = 11,
+	LightRed = 12,
+	LightMagenta = 13,
+	Yellow = 14,
+	White = 15
+};
+
+void SetColor(ConsoleColor text, ConsoleColor background)
+{
+	SetConsoleTextAttribute(hStdOut, (WORD)((background << 4) | text));
+}
+
+
 void Tittle()
 {
 	cout << ("\n\n\n\t\t\t************ 15  ************\n\n"
